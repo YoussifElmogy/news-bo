f3239046bc436c9da667aa41c21d02c7683e4db4
Youssef Elmogy <Youssef.elmogy@linkdev.com>
2025-12-12 16:29:49 +0200

    Refactor newsSchema by removing validation for Arabic and English titles and descriptions

--- src/components/form/news/newsSchema.js
+++ src/components/form/news/newsSchema.js
@@ -1,22 +1,6 @@
 import * as yup from 'yup';
 
 const newsSchema = yup.object().shape({
-  titleArabic: yup
-    .string()
-    .required('Arabic title is required')
-    .min(3, 'Arabic title must be at least 3 characters'),
-  titleEnglish: yup
-    .string()
-    .required('English title is required')
-    .min(3, 'English title must be at least 3 characters'),
-  descriptionArabic: yup
-    .string()
-    .required('Arabic description is required')
-    .min(10, 'Arabic description must be at least 10 characters'),
-  descriptionEnglish: yup
-    .string()
-    .required('English description is required')
-    .min(10, 'English description must be at least 10 characters'),
   category: yup
     .string()
     .required('Category is required')
